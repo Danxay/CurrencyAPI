d66c1b1db5221dbcb7d2143c0ebf07ccc4e625e4
Danxay <66563593+Danxay@users.noreply.github.com>
2021-10-31 20:18:53 +0600

    Update settings.py

--- Currency/settings.py
+++ Currency/settings.py
@@ -129,6 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'CurrencyAPI.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     'EXCEPTION_HANDLER': 'Currency.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
